6a93307c516adebc175970dfccb3dbaa42ff026d
Steven Van Iseghem <steven.van.iseghem@outlook.com>
2019-01-17 20:06:40 +0100

    Adding Error Routing

--- WebApplicationDemo/Controllers/ErrorController.cs
+++ WebApplicationDemo/Controllers/ErrorController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplicationDemo.Controllers
 {
+	[Route("Error")]
 	public class ErrorController
 	{
+		[Route("Support")]
 		public string Support()
 		{
 			return "Content not Found. Contact Support.";
